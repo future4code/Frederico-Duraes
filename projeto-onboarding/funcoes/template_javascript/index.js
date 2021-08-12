486ec18969d0045817fba3d00b5c6cef474bc212
fredduraes <83043328+fredduraes@users.noreply.github.com>
2021-08-12 13:37:16 -0400

    Add files via upload

---
+++ projeto-onboarding/funcoes/template_javascript/index.js
@@ -0,0 +1,131 @@
+//1.Leia o código abaixo
+//a O que vai ser impresso no console?
+
+//b O que aconteceria se retirasse os dois `console.log` 
+
+//e simplesmente invocasse a função `minhaFuncao(2)` 
+
+//e `minhaFuncao(10)`? O que apareceria no console?
+
+
+/*function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+/*console.logminhaFuncao(2)
+/*console.logminhaFuncao(10)
+
+R -  A - 10 , e 50 
+R - B - nao vai aparecer nada 
+R - C - aparera minhaFuncao(2) 10 e minhaFuncao(10)  50 */
+
+
+
+/*2 .Leia o código abaixo
+
+b. Determine qual será a saída no console 
+
+para cada uma das 3 
+
+entradas do usuário:
+
+i.   Eu gosto de cenoura
+
+ii.  CENOURA é bom pra vista
+
+iii. Cenouras crescem na terra
+
+
+let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)
+
+R - verdadeira - true */
+
+
+
+/*1 .Escreva as funções explicadas abaixo:
+a) A função não deve receber nenhum parâmetro 
+
+e deve imprimir uma mensagem falando algumas 
+
+informações sobre você, como: 
+
+
+function nome() {
+    const minhaIdade = prompt("meu nome", nome)
+}
+
+console.log(nome)
+
+R - nao entendi esse exercico pesquisei na internet mais nao entendi */
+
+
+
+
+/*2 .Escreva as funções explicadas abaixo:
+
+/*a) Escreva uma função que receba 2 números como parâmetros,
+
+ e, dentro da função, faça a soma das duas entradas e retorne 
+
+ o resultado. Invoque a função e imprima no console o resultado.
+
+b) Faça uma função que recebe 2 números e retorne um booleano 
+
+que informa se o primeiro número é **maior ou igual** ao segundo.
+
+c) Escreva uma função que receba um número 
+
+e devolva um booleano indicando se ele é par ou não
+
+d) Faça uma função que recebe uma mensagem 
+
+(`string`) como parâmetro e imprima o tamanho dessa mensagem,
+
+ juntamente com uma versão dela em letras maiúsculas.
+
+
+
+function numero1(soma) {
+    return soma + 2
+
+}
+
+console.log(numero1(2))
+console.log(numero1(3))
+
+
+R- fiquei perdido depois dessa function*/
+
+
+
+/*3.Crie uma função para cada uma das operações básicas 
+
+(soma, subtração, multiplicação e divisão). 
+
+Em seguida, peça para o usuário inserir dois números
+
+ e chame essas 4 funções com esses valores inputados 
+
+ pelo usuário sendo o argumento. Por fim, mostre no console
+
+o resultado das operações:
+
+Números inseridos: 30 e 3
+Soma: 33
+Diferença: 27
+Multiplicação: 90
+Divisão: 10 
+
+
+function soma = prompt("Digite um numero") {
+    const subtracao
+}
+
+R - nao entendi */
